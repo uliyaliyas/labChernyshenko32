0feb3065e91b0b6d4a54b64fa49a30af0f856d9d
uliyana <uliyaiyaas@gmail.com>
2023-12-27 23:23:16 +0200

    Добавьте файлы проекта.

---
+++ labChernyshenko32/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите широту (F):");
+double F = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите долготу (D):");
+double D = double.Parse(Console.ReadLine()!);
+
+if (F >= 0)
+{
+    Console.WriteLine("Точка находится в северном полушарии.");
+}
+else
+{
+    Console.WriteLine("Точка находится в южном полушарии.");
+}
+
+if (D >= 0)
+{
+    Console.WriteLine("Точка находится в восточном полушарии.");
+}
+else
+{
+    Console.WriteLine("Точка находится в западном полушарии.");
+}
